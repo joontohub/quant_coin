138a6b26dfbcfc00d244645b0b12bf954d995919
joontohub <joonto7828@gmail.com>
2021-04-07 15:43:22 +0900

    add atr

--- rsi_macd/supertrend.py
+++ rsi_macd/supertrend.py
@@ -35,6 +35,67 @@ Usage :
 
 # I need to change 'date', 'open', 'high', 'low', 'close', 'volume' column
 
+
+
+def EMA(df, base, target, period, alpha=False):
+    """
+    Function to compute Exponential Moving Average (EMA)
+    
+    Args :
+        df : Pandas DataFrame which contains ['date', 'open', 'high', 'low', 'close', 'volume'] columns
+        base : String indicating the column name from which the EMA needs to be computed from
+        target : String indicates the column name to which the computed data needs to be stored
+        period : Integer indicates the period of computation in terms of number of candles
+        alpha : Boolean if True indicates to use the formula for computing EMA using alpha (default is False)
+        
+    Returns :
+        df : Pandas DataFrame with new column added with name 'target'
+    """
+
+    con = pd.concat([df[:period][base].rolling(window=period).mean(), df[period:][base]])
+    
+    if (alpha == True):
+        # (1 - alpha) * previous_val + alpha * current_val where alpha = 1 / period
+        df[target] = con.ewm(alpha=1 / period, adjust=False).mean()
+    else:
+        # ((current_val - previous_val) * coeff) + previous_val where coeff = 2 / (period + 1)
+        df[target] = con.ewm(span=period, adjust=False).mean()
+    
+    df[target].fillna(0, inplace=True)
+    return df
+
+def ATR(df, period, ohlc=['Open', 'High', 'Low', 'Close']):
+    """
+    Function to compute Average True Range (ATR)
+    
+    Args :
+        df : Pandas DataFrame which contains ['date', 'open', 'high', 'low', 'close', 'volume'] columns
+        period : Integer indicates the period of computation in terms of number of candles
+        ohlc: List defining OHLC Column names (default ['Open', 'High', 'Low', 'Close'])
+        
+    Returns :
+        df : Pandas DataFrame with new columns added for 
+            True Range (TR)
+            ATR (ATR_$period)
+    """
+    atr = 'ATR_' + str(period)
+
+    # Compute true range only if it is not computed and stored earlier in the df
+    if not 'TR' in df.columns:
+        df['h-l'] = df[ohlc[1]] - df[ohlc[2]]
+        df['h-yc'] = abs(df[ohlc[1]] - df[ohlc[3]].shift())
+        df['l-yc'] = abs(df[ohlc[2]] - df[ohlc[3]].shift())
+         
+        df['TR'] = df[['h-l', 'h-yc', 'l-yc']].max(axis=1)
+         
+        df.drop(['h-l', 'h-yc', 'l-yc'], inplace=True, axis=1)
+
+    # Compute EMA of true range using ATR formula after ignoring first row
+    EMA(df, 'TR', atr, period, alpha=True)
+    
+    return df
+
+
 def SuperTrend(df, period, multiplier, ohlc=['Open', 'High', 'Low', 'Close']):
     """
     Function to compute SuperTrend
